5d511428ea4466f27ce0f55bf05751c153c3ff55
Hristo Vrigazov <hvrigazov@gmail.com>
2017-06-16 14:08:56 +0300

    Basic implementation of PID controller without any special tuning

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,22 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp; // proportional
+  this->Ki = Ki; // integral
+  this->Kd = Kd; // differential
+
+  p_error = 1.0;  // proportional cte
+  i_error = 0.0; // integral cte
+  d_error = 0.0; // differential cte
 }
 
 void PID::UpdateError(double cte) {
+  d_error = cte - p_error;
+  p_error = cte;
+  i_error += cte;
 }
 
 double PID::TotalError() {
+  return -Kp * p_error - Kd * d_error - Ki * i_error;
 }
 
